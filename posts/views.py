76ba222441ba2929d6b51e4e83bdc916eb82388f
ericksoftware <erickcodessoftware@gmail.com>
2025-05-30 23:17:07 -0700

    final report

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+from .models import Post
+from django.urls import reverse_lazy
 
-# Create your views here.
+class PostUpdateView(UpdateView):
+    template_name = 'posts/edit.html'
+    model = Post
+    fields = ["title", "subtitle", "body", "author"]
+
+class PostDeleteView(DeleteView):
+    template_name = 'posts/delete.html'
+    model = Post
+    success_url = reverse_lazy("post_list")
+
+class PostListView(ListView):
+    template_name = 'posts/post_list.html'
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = 'posts/post_detail.html'
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = 'posts/post_form.html'
+    model = Post
+    fields = ["title", "subtitle", "body", "author"]
