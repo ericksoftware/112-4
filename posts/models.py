4ff1cb8cc4d0443084659822ab8b361cb2239a87
ericksoftware <erickcodessoftware@gmail.com>
2025-06-01 06:55:16 -0700

    Assignment 1

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,23 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+class Status(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
     body = models.TextField()
     author = models.ForeignKey(
         get_user_model(),
+        on_delete=models.CASCADE
+    )
+    status = models.ForeignKey(
+        Status,
         on_delete=models.CASCADE
     )
     created_on = models.DateTimeField(auto_now_add=True)
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,25 @@
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'posts/edit.html'
     model = Post
-    fields = ["title", "subtitle", "body", "author"]
+    fields = ["title", "subtitle", "body", "author", "status"]
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = 'posts/delete.html'
     model = Post
     success_url = reverse_lazy("post_list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 class PostListView(ListView):
     template_name = 'posts/post_list.html'
@@ -20,7 +29,15 @@ class PostDetailView(DetailView):
     template_name = 'posts/post_detail.html'
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     template_name = 'posts/post_form.html'
     model = Post
-    fields = ["title", "subtitle", "body", "author"]
+    fields = ["title", "subtitle", "body", "status"]
+
+    def test_func(self):
+        return self.request.user.is_authenticated
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
